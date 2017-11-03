35c4610361efed5023d1181ec90af178d0c1ff7a
Masahiro Yagyu <gyu7372@gmail.com>
2017-11-03 10:43:42 +0900

    メッセージ送信機能
    メッセージ送信機能

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,16 @@
 class MessagesController < ApplicationController
   def index
   end
+
+  def create
+    message = current_user.messages.new(message_params)
+    if message.save
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:message, :group_id)
+  end
 end
