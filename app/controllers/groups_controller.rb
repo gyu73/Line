22edb245911be9549d9c67f2420297f15a53465d
Masahiro Yagyu <gyu7372@gmail.com>
2017-11-10 13:56:18 +0900

    modify6
    modify6

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,7 @@
 class GroupsController < ApplicationController
   def index
     @groups = current_user.groups.includes(:user_groups).includes(:users).includes(:messages).order('message_created_at DESC')
+    @counts = count_not_read_messages(@groups)
   end
 
   def create
@@ -14,14 +15,18 @@ class GroupsController < ApplicationController
   end
 
   def show
-    @current_group = Group.find(params[:id])
-    @messages = Message.where(group_id: params[:id]).includes(:user).includes(:already_reads)
-    @users = @current_group.users
-    @new_message = Message.new
-    AlreadyRead.where(is_read: false, message_id: @messages.ids).where.not(user_id: current_user.id).update(is_read: true)
-    respond_to do |format|
-      format.html
-      format.json { render json: update_new_messages_json }
+    if Group.last.id >= params[:id].to_i && current_user.groups.ids.include?(Group.find(params[:id]).id)
+      @current_group = Group.find(params[:id])
+      @messages = Message.where(group_id: params[:id]).includes(:user).includes(:already_reads)
+      @users = @current_group.users
+      @new_message = Message.new
+      AlreadyRead.where(is_read: false, message_id: @messages.ids).where.not(user_id: current_user.id).update(is_read: true)
+        respond_to do |format|
+          format.html
+          format.json { render json: update_new_messages_json }
+        end
+    else
+      redirect_to groups_path
     end
   end
 
@@ -35,14 +40,27 @@ class GroupsController < ApplicationController
     params.permit(:LastMessageId, :id)
   end
 
+  def count_not_read_messages(groups)
+    counts = []
+    groups.each do |group|
+      messages = group.messages.includes(:already_reads).where.not(user_id: current_user.id)
+      count = 0
+      messages.each do |message|
+        count += 1 unless message.already_reads[0].is_read
+      end
+      counts << count
+    end
+    return counts
+  end
+
   def update_new_messages_json
     update_messages = Message.where(group_id: last_message_params[:id]).where("id > ?", last_message_params[:LastMessageId])
     update_messages_json = []
     update_messages.each do |up_mes|
       if up_mes.user_id == current_user.id
-        update_messages_json << { message_id: up_mes.id, name: up_mes.user.name, created_at: up_mes.created_at.strftime('%H:%M:%S'), message: up_mes.message, user_info: "current_user" }
+        update_messages_json << { message_id: up_mes.id, name: up_mes.user.name, created_at: up_mes.created_at.strftime('%H:%M:%S'), message: up_mes.message, is_read: up_mes.already_reads[0].is_read, user_info: "current_user" }
       else
-        update_messages_json << { message_id: up_mes.id, name: up_mes.user.name, created_at: up_mes.created_at.strftime('%H:%M:%S'), message: up_mes.message, user_info: "not_current_user" }
+        update_messages_json << { message_id: up_mes.id, name: up_mes.user.name, created_at: up_mes.created_at.strftime('%H:%M:%S'), message: up_mes.message, is_read: up_mes.already_reads[0].is_read, user_info: "not_current_user" }
       end
     end
     return update_messages_json
